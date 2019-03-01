90255acd2264564ab8a15731542191a1dd94db80
Fightnight7 <48089821+Fightnight7@users.noreply.github.com>
2019-03-01 11:54:07 +0200

    Add files via upload

--- 1.js
+++ 1.js
@@ -3,50 +3,47 @@ n1 = document.getElementById("num1");
 n2 = document.getElementById("num2");
 out = document.getElementById("out");
 
-function plus(){
+function getNum(){
     num1 = n1.value;
     num1 = parseInt(num1);
     num2 = n2.value;
     num2 = parseInt(num2);
-    result = num1+num2;
-    out.innerHTML = result;
+}
+
+function clear(){
     n1.value = "";
     n2.value = "";
+}
+
+function plus(){
+    getNum ();
+    result = num1+num2;
+    out.innerHTML = result;
+    clear();
 }
 
 function minus(){
-   num1 = n1.value;
-    num1 = parseInt(num1);
-    num2 = n2.value;
-    num2 = parseInt(num2);
+    getNum ();
     result = num1-num2;
     out.innerHTML = result;
-    n1.value = "";
-    n2.value = "";
+    clear();
 }
 
 function multiply(){
-    num1 = n1.value;
-    num1 = parseInt(num1);
-    num2 = n2.value;
-    num2 = parseInt(num2);
+    getNum ();
     result = num1*num2;
     out.innerHTML = result;
-    n1.value = "";
-    n2.value = "";
+    clear();
 }
 
 function divide(){
-    num1 = n1.value;
-    num1 = parseInt(num1);
-    num2 = n2.value;
-    num2 = parseInt(num2);
+    getNum ();
     if (num2 == 0){
         alert ("Division by 0 is imposible");
     }
-    else {result = num1/num2;
+    else {
+        result = num1/num2;
         out.innerHTML = result;
-        n1.value = "";
-        n2.value = "";
+        clear();
     }
 }
